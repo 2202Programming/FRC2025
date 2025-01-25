76788890727fe5f536cb6e79e23a37b414f1ec3c
Gavin <gavin.r.longley@gmail.com>
2025-01-25 10:07:59 -0600

    added the bare bones shell of a climber and end effector subsystem
    OK so these have a lot that still needs to get done, but these things cant really get done until we have a robot to test them on :). We could add pid constants, general heights in the elevator subsystem, and maybe start commands.

--- src/main/java/frc/robot2025/subsystems/Elevator_Subsystem.java
+++ src/main/java/frc/robot2025/subsystems/Elevator_Subsystem.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot2025.subsystems.sensors;
+package frc.robot2025.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.spark.SparkMax;
@@ -14,9 +14,14 @@ public class Elevator_Subsystem extends SubsystemBase {
   /** Creates a new Elevator_Subsystem. */
 
   private enum Levels {
-    LCoral, LOne, LTwo, LThree, LFour
+    LCoral, LOne, LTwo, LThree, LFour //may add heights that corralate to these, double check with andrew on how to do
   };
   private SparkMax Elevator_Motor = new SparkMax(CAN.Elevator, SparkMax.MotorType.kBrushless);
+  private SparkMax Elevator_Motor2 = new SparkMax(CAN.Elevator2, SparkMax.MotorType.kBrushless); 
+  //what kind of sensors will we have on this bot. Not fully sure yet. Double check with mechanical
+  private double elevator_speed; //what will "speed" be in? I'm thinking inches per second right now \*_*/
+  private double elevator_height; //in inches
+  
 
   public Elevator_Subsystem() {
 
@@ -27,6 +32,17 @@ public class Elevator_Subsystem extends SubsystemBase {
     // This method will be called once per scheduler run
   }
    
-  
+  public double get_Elevator_Height() {
+    return 0.0; //some sort of calculation, or durring the peridic we keep updating the positon of the elevator.
+  }
+
+  public void Elevator_Set_ToHeight(double height) {
+
+  }
+
+  public void set_Elevator_Speed(double speed){ 
+    this.elevator_speed = speed;
+  }
+
   
 }
