07483122639e2f4e221a80bbc6d9ca7f1fb7a97f
Gavin <gavin.r.longley@gmail.com>
2025-02-01 10:38:30 -0600

    Fixed my nonsense, added neo servo. Still need to do height stuff

--- src/main/java/frc/robot2025/subsystems/Elevator_Subsystem.java
+++ src/main/java/frc/robot2025/subsystems/Elevator_Subsystem.java
@@ -4,55 +4,58 @@
 
 package frc.robot2025.subsystems;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-
 import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib2202.util.NeoServo;
+import frc.lib2202.util.PIDFController;
 import frc.robot2025.Constants.CAN;
 
 
 public class Elevator_Subsystem extends SubsystemBase {
   /** Creates a new Elevator_Subsystem. */
 
+  //get once we have prototype (USE ELLANA'S CODE AS EXAMPLE)
   private enum Levels {
     LCoral, LOne, LTwo, LThree, LFour //may add heights that corralate to these, double check with andrew on how to do
-  };
-  private final SparkMax Elevator_Motor;
-  private final SparkMax Elevator_Motor2; 
-  private SparkClosedLoopController elevatorPid;
+  }; 
   private final PIDController elevatorPidController;
-  private NeoServo elevator_Servo;
+  private final PIDFController elevatorPIDF;
+  private NeoServo elevator_Servo1;
+  private SparkMax elevator_motor2;
+  private SparkMaxConfig motor2_Config;
   //what kind of sensors will we have on this bot. Not fully sure yet. Double check with mechanical
-  private double elevator_velecity; // cm/sec
   private double elevator_height; //in cm
   private double elevator_height_setpoint; //in cm
-  private SparkMaxConfig elevator_config;
 
   private int gear_Ratio = 5;  //throw in constants?
   private int chain_Ratio = 314159;
   
 
   public Elevator_Subsystem() {
-    Elevator_Motor = new SparkMax(CAN.Elevator, SparkMax.MotorType.kBrushless);
-    Elevator_Motor2 = new SparkMax(CAN.Elevator, SparkMax.MotorType.kBrushless);
-    elevator_height = 0;
-    elevator_config = new SparkMaxConfig();
     elevatorPidController = new PIDController(0, 0, 0);
-
-    elevator_config
-      .idleMode(IdleMode.kBrake);
+    elevatorPIDF = new PIDFController(0, 0, 0, 0);
+    elevator_Servo1 = new NeoServo(CAN.Elevator ,elevatorPidController, elevatorPIDF, false);
+    elevator_motor2 = new SparkMax(-10, MotorType.kBrushless); //TODO change CAN ID
     
-    Elevator_Motor.configure(elevator_config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    Elevator_Motor2.configure(elevator_config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-      
+    //lines 51-55 config motor 2 the same as the first motor 
+    motor2_Config = new SparkMaxConfig();
+        motor2_Config.inverted(false)
+               .idleMode(IdleMode.kBrake);
+    motor2_Config.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder) 
+                .outputRange(-1.0, 1.0);
+    
+    elevator_motor2.configure(motor2_Config,ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    motor2_Config.follow(CAN.Elevator); //motor 2 follows the servo's behavior
+    elevator_height = 0;
+  
   }
 
   @Override
@@ -61,15 +64,13 @@ public class Elevator_Subsystem extends SubsystemBase {
   }
    
   public double get_Elevator_Height() {
-    return elevator_height; //some sort of calculation during the peridic, where we keep updating the positon of the elevator.
+    return elevator_Servo1.getPosition(); //some sort of calculation needed
   }
 
   public void Elevator_Set_ToHeight(double height) {
     elevator_height_setpoint = height;
-  }
-
-  public void set_Elevator_Speed(double speed){ 
-    this.elevator_velecity = speed;
+    elevator_Servo1.setSetpoint(height);
+  
   }
 
   public double get_elevator_setpoint() {
@@ -77,7 +78,7 @@ public class Elevator_Subsystem extends SubsystemBase {
   }
 
   public double get_elevator_speed() {
-    return elevator_velecity;
+    return elevator_Servo1.getVelocity();
   }
 
   
