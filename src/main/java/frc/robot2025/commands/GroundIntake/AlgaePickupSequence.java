b107cf4b50d94e839e2a150afb0ec5d99608ad6a
kamith <kvml96@gmail.com>
2025-02-01 13:10:46 -0600

    Elena wrote this......

--- src/main/java/frc/robot2025/commands/GroundIntake/AlgaePickupSequence.java
+++ src/main/java/frc/robot2025/commands/GroundIntake/AlgaePickupSequence.java
@@ -28,40 +28,41 @@ public class AlgaePickupSequence extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-
-    phase = Phase.Rest;
+    phase = Phase.AlgaePickup;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     switch(phase){
-      case Rest:
-
-      break;
-
       case AlgaePickup:
-      groundIntake.setGroundIntakePosition(Position.ALGAE_PICKUP);
-      groundIntake.setGroundIntakeWheelSpeed(1.0);
-      phase = Phase.WaitForAlgae;
-      break;
+        groundIntake.setGroundIntakePosition(Position.ALGAE_PICKUP);
+        groundIntake.setGroundIntakeWheelSpeed(1.0);
+        phase = Phase.WaitForAlgae;
+        break;
 
       case WaitForAlgae:
-      hasAlgae = groundIntake.senseGamePiece();
-      phase = hasAlgae ? Phase.AlgaeRest : Phase.WaitForAlgae;
-      break;
+        hasAlgae = groundIntake.senseGamePiece();
+        phase = hasAlgae ? Phase.AlgaeRest : Phase.WaitForAlgae;
+        break;
 
       case AlgaeRest:
-      groundIntake.setGroundIntakePosition(Position.ALGAE_REST);
-      groundIntake.setGroundIntakeWheelSpeed(0.0);
-      break;
-      
+        groundIntake.setGroundIntakePosition(Position.ALGAE_REST);
+        groundIntake.setGroundIntakeWheelSpeed(0.0);
+        phase = Phase.Finished;
+        break;
+
+      case Finished:
+        break; 
     }
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    groundIntake.setGroundIntakePosition(groundIntake.senseGamePiece() ? Position.ALGAE_REST : Position.Zero);
+    groundIntake.setGroundIntakeWheelSpeed(0.0);
+  }
 
   // Returns true when the command should end.
   @Override
