d4bf8a01edcd1cc776b3eebf52550fdef74d716f
kamith <kvml96@gmail.com>
2025-02-01 14:30:23 -0600

    initialize stuff

--- src/main/java/frc/robot2025/commands/GroundIntake/CoralPickupSequence.java
+++ src/main/java/frc/robot2025/commands/GroundIntake/CoralPickupSequence.java
@@ -5,25 +5,53 @@
 package frc.robot2025.commands.GroundIntake;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot2025.subsystem.GroundIntake.Position;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class CoralPickupSequence extends Command {
-  /** Creates a new CoralPickup. */
+
+  public enum State {
+    WaitForCoral,
+    CoralRest,
+    Finished
+  }
+  State state;
+  final GroundIntake groundIntake;
+  boolean hasCoral;
+
   public CoralPickupSequence() {
-    // Use addRequirements() here to declare subsystem dependencies.
+    this.groundIntake = RobotContainer.getSubsystem(GroundIntake.class);
+    this.hasCoral = false;
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    groundIntake.setGroundIntakePosition(Position.CORAL_PICKUP);
+    groundIntake.setGroundIntakeWheelSpeed(1.0);
+    state = State.WaitForCoral;
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    switch(state) {
+      case WaitForCoral:
+        break;
+
+      case CoralRest:
+        break;
+
+      case Finished:
+        break;
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    
+  }
 
   // Returns true when the command should end.
   @Override
