d6b817a628e988daa2ecc6d3350083202cc7bea2
Gavin <gavin.r.longley@gmail.com>
2025-02-13 19:39:37 -0600

    Mr. L helped add the command that tells us "is at setpoint"
    Listen to Good Kid

--- src/main/java/frc/robot2025/subsystems/LinearServo.java
+++ src/main/java/frc/robot2025/subsystems/LinearServo.java
@@ -5,28 +5,36 @@
 package frc.robot2025.subsystems;
 
 import edu.wpi.first.wpilibj.PWM;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LinearServo extends SubsystemBase {
   /** Creates a new linearServo. */
   PWM servo;
 
-  public LinearServo() {
+  double EtoETime = 3.0; //sec, time from 0 - 1.0 traveled
+  double prevPos;
+  double lastCommandTime;
+  double timeToFinish;
+
+  public LinearServo(double initPos) {
     //works on a scale of 0-1, and autoclamps 
     servo = new PWM(0);
-    System.out.println(servo.getChannel());
-  }
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+    prevPos = initPos;
+    setPos(initPos);
   }
 
   public void setPos(double pos) {
+    timeToFinish = EtoETime * Math.abs(pos - prevPos) + Timer.getFPGATimestamp();
     servo.setPosition(pos);
   } 
   
-  public double getPos() {
-    return servo.getPosition();
+
+  public double getEstimatedTime() {
+    return timeToFinish;
+  }
+
+  public boolean isAtSetpoint() {
+    return Timer.getFPGATimestamp() >= timeToFinish;
   }
 }
