4ff9819d5d2f6a3042c86a8ea9de9d53d5785c3a
Beast Software <27guptap1@elmbrookstudents.org>
2025-03-07 18:54:05 -0600

    debugged 2024 robot drivetrain; works fine now
    -- BG

--- src/main/java/frc/robot2025/RobotSpec_test2024.java
+++ src/main/java/frc/robot2025/RobotSpec_test2024.java
@@ -151,26 +151,26 @@ public class RobotSpec_test2024 implements IRobotSpec {
     /// LL is on Front Left corner
     public ModuleConfig[] getModuleConfigs() {
         ModuleConfig[] modules = new ModuleConfig[4];
-
-        modules[CornerID.FrontLeft.getIdx()] = new ModuleConfig(CornerID.FrontLeft,
-                30, 26, 27,
-                -114.785 - 90.0)
-                .setInversions(true, true, false);
-
-        modules[CornerID.FrontRight.getIdx()] = new ModuleConfig(CornerID.FrontRight,
-                31, 20, 21,
-                -115.752 - 90.0)
-                .setInversions(true, true, false);
         
-        modules[CornerID.BackLeft.getIdx()] = new ModuleConfig(CornerID.BackLeft,
-                29, 24, 25,
-                -125.595 - 90.0)
-                .setInversions(false, true, false);
+        modules[CornerID.FrontLeft.getIdx()] = new ModuleConfig(CornerID.BackRight,
+        28, 22, 23,
+        28.125 + 90.0)
+        .setInversions(false, true, false);
 
-        modules[CornerID.BackRight.getIdx()] = new ModuleConfig(CornerID.BackRight,
-                28, 22, 23,
-                28.125 - 90.0)
-                .setInversions(false, true, false);
+        modules[CornerID.FrontRight.getIdx()] = new ModuleConfig(CornerID.BackLeft,
+        29, 24, 25,
+        -125.595 + 90.0)
+        .setInversions(false, true, false);
+        
+        modules[CornerID.BackLeft.getIdx()] = new ModuleConfig(CornerID.FrontRight,
+        31, 20, 21,
+        -115.752 + 90.0)
+        .setInversions(true, true, false);
+
+        modules[CornerID.BackRight.getIdx()] = new ModuleConfig(CornerID.FrontLeft,
+        30, 26, 27,
+        -114.785 + 90.0)
+        .setInversions(true, true, false);
                 
         return modules;
     }
@@ -214,7 +214,7 @@ public class RobotSpec_test2024 implements IRobotSpec {
         // ConfigureAutobuilder uses default pids and looks up SwereveDrivetrain from RobotContainer
         ///AutoPPConfig.ConfigureAutoBuilder();
         /// 
-        return RegisteredCommands.RegisterCommands();
+        return null; //RegisteredCommands.RegisterCommands();
     }
 
     @Override
