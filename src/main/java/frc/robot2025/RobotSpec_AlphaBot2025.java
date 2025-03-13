291ecd6a5d8985dae16226baf34d3b643117bd76
DerekL <derek@adminnet.us>
2025-03-13 00:35:52 -0500

    disable test binding in integration

--- src/main/java/frc/robot2025/RobotSpec_AlphaBot2025.java
+++ src/main/java/frc/robot2025/RobotSpec_AlphaBot2025.java
@@ -41,7 +41,6 @@ import frc.robot2025.subsystems.Sensors_Subsystem;
 import frc.robot2025.subsystems.SignalLight;
 import frc.robot2025.subsystems.VisionPoseEstimator;
 import frc.robot2025.subsystems.Wrist;
-import frc.robot2025.testBindings.DPLPathTest;
 import frc.robot2025.utils.UXTrim;
 
 public class RobotSpec_AlphaBot2025 implements IRobotSpec {
@@ -196,7 +195,7 @@ public class RobotSpec_AlphaBot2025 implements IRobotSpec {
     // comment out any conflicting bindings. Try not to push with your bindings
     // active. Just comment them out.
     
-    DPLPathTest.myBindings(dc); 
+    //DPLPathTest.myBindings(dc); 
     //ElevTest.myBindings(dc);
     //EndEffectorTest.myBindings(dc);
     //GITest.myBindings(dc);
