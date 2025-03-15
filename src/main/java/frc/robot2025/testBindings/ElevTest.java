3fc38170f0fb630f148760b7bc7eeb91be743fe6
Noah Ren <nren2025@gmail.com>
2025-03-15 10:11:23 -0500

    branch name

--- src/main/java/frc/robot2025/testBindings/ElevTest.java
+++ src/main/java/frc/robot2025/testBindings/ElevTest.java
@@ -1,24 +1,50 @@
 package frc.robot2025.testBindings;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.lib2202.builder.RobotContainer;
+import frc.lib2202.command.pathing.MoveToPose;
+import frc.lib2202.subsystem.OdometryInterface;
 import frc.lib2202.subsystem.hid.HID_Subsystem;
+import frc.lib2202.subsystem.swerve.DriveTrainInterface;
 import frc.robot2025.commands.ElevatorCalibrate;
 import frc.robot2025.commands.EndEffectorPercent;
 import frc.robot2025.commands.testElevatorVelComd;
 import frc.robot2025.commands.DropSequenceBaseCommands.ReleaseCoral;
 import frc.robot2025.commands.DropSequenceBaseCommands.setElevatorSetpoint;
 import frc.robot2025.commands.DropSequenceBaseCommands.setWristPos;
-import frc.robot2025.subsystems.Elevator_Subsystem;
+// import frc.robot2025.subsystems.Elevator_Subsystem;
 import frc.robot2025.subsystems.Elevator_Subsystem.Levels;
+import frc.robot2025.subsystems.SignalLight;
+import frc.robot2025.subsystems.VisionPoseEstimator;
 import frc.robot2025.subsystems.Wrist;
 
 public class ElevTest {
+    static OdometryInterface odo;
+     static String OdometryName = VisionPoseEstimator.class.getSimpleName();
+    static DriveTrainInterface sdt;
+    static SignalLight signal;
+
+    static Command move(){
+        Pose2d currentPose = odo.getPose(); // field coords
+            // add 1m forward, field not robot.
+            Pose2d target = new Pose2d(currentPose.getX(),
+                    currentPose.getY() + 0.127, currentPose.getRotation());
+            // force a color
+            if (signal != null)
+                signal.setLight(SignalLight.Color.BLUE);
+            // calc path
+            Command cmd = new MoveToPose(OdometryName, target);
+            if (signal != null)
+            cmd = cmd.andThen(signal.getColorCommand(SignalLight.Color.OFF)); 
+            return cmd;
+    }
     public static void myBindings(HID_Subsystem dc) {
-        Elevator_Subsystem elevator_Subsystem = RobotContainer.getSubsystem(Elevator_Subsystem.class);
+        // Elevator_Subsystem elevator_Subsystem = RobotContainer.getSubsystem(Elevator_Subsystem.class);
         Wrist wrist = RobotContainer.getSubsystem(Wrist.class);
         // get an xbox controller for the operator, or null
         CommandXboxController opr = (dc.Operator() instanceof CommandXboxController)
@@ -30,6 +56,16 @@ public class ElevTest {
 
         opr.b().onTrue(new SequentialCommandGroup (
             new ParallelCommandGroup(
+                new InstantCommand(() -> {
+                    Pose2d currentPose = odo.getPose(); // field coords
+                    // add 1m forward, field not robot.
+                    Pose2d target = new Pose2d(currentPose.getX(),
+                            currentPose.getY() + 0.127, currentPose.getRotation());
+                    // calc path
+                    Command cmd = new MoveToPose(OdometryName, target);      
+                    cmd.setName("moveto-backup");     
+                    cmd.schedule();
+                }),
                 new setElevatorSetpoint(Levels.LTwo).withTimeout(5.0),
                 new setWristPos(true)),
                 new ReleaseCoral(),
@@ -38,6 +74,16 @@ public class ElevTest {
                 new setElevatorSetpoint(Levels.PickUp)
         ));
         opr.y().onTrue(new SequentialCommandGroup (
+            new InstantCommand(() -> {
+                Pose2d currentPose = odo.getPose(); // field coords
+                // add 1m forward, field not robot.
+                Pose2d target = new Pose2d(currentPose.getX(),
+                        currentPose.getY() + 0.127, currentPose.getRotation());
+                // calc path
+                Command cmd = new MoveToPose(OdometryName, target);      
+                cmd.setName("moveto-backup");     
+                cmd.schedule();
+            }),
             new ParallelCommandGroup(
             new setElevatorSetpoint(Levels.LThree).withTimeout(2.0),
             new setWristPos(true)),
@@ -48,8 +94,18 @@ public class ElevTest {
         ));
         opr.leftBumper().onTrue(new SequentialCommandGroup (
             new ParallelCommandGroup(
-            new setElevatorSetpoint(Levels.LFour).withTimeout(3.0),
-            new setWristPos(0.05)), //position for L4 drop
+                new InstantCommand(() -> {
+                    Pose2d currentPose = odo.getPose(); // field coords
+                    // add 1m forward, field not robot.
+                    Pose2d target = new Pose2d(currentPose.getX(),
+                            currentPose.getY() + 0.127, currentPose.getRotation());
+                    // calc path
+                    Command cmd = new MoveToPose(OdometryName, target);      
+                    cmd.setName("moveto-backup");     
+                    cmd.schedule();
+                }),
+            new setElevatorSetpoint(Levels.LFour).withTimeout(3.0)),
+            new setWristPos(0.05), //position for L4 drop
             new ReleaseCoral(),
             new setWristPos(false).withTimeout(1.0),
             new setElevatorSetpoint(Levels.PickUp)
@@ -60,7 +116,23 @@ public class ElevTest {
         opr.leftTrigger().onTrue(new InstantCommand(() -> {
             wrist.setPos(wrist.pickup);
         }));
-
+        opr.povUp().onTrue(new InstantCommand(() -> {
+            Pose2d currentPose = odo.getPose(); // field coords
+            // add 1m forward, field not robot.
+            Pose2d target = new Pose2d(currentPose.getX(),
+                    currentPose.getY() + 0.127, currentPose.getRotation());
+            // force a color
+            if (signal != null)
+                signal.setLight(SignalLight.Color.BLUE);
+            // calc path
+            Command cmd = new MoveToPose(OdometryName, target);
+            // turn signal off after our move, if we have a signal object
+            if (signal != null)
+                cmd = cmd.andThen(signal.getColorCommand(SignalLight.Color.OFF));
+            cmd.setName("moveto-fwd/w signal"); 
+            cmd.schedule();
+        }));
+        opr.povDown().onTrue(move());
         
         // opr.y().onTrue(new ClimberPosition(0.0));
         // opr.b().onTrue(new InstantCommand(() -> {
@@ -69,16 +141,16 @@ public class ElevTest {
         // opr.a().onTrue(new InstantCommand(() -> {
         // elevator_Subsystem.setHeight(90.0);
         // }));
-        opr.povDown().onTrue(new InstantCommand(() -> {
-            wrist.setPos(0.3);
-        }));
-        opr.povLeft().onTrue(new InstantCommand(() -> {
-            wrist.setPos(0.05);
-        }));
-        opr.povRight().onTrue(new InstantCommand(() -> {
-            elevator_Subsystem.setHeight(0.6);
+        // opr.povDown().onTrue(new InstantCommand(() -> {
+        //     wrist.setPos(0.3);
+        // }));
+        // opr.povLeft().onTrue(new InstantCommand(() -> {
+        //     wrist.setPos(0.05);
+        // }));
+        // opr.povRight().onTrue(new InstantCommand(() -> {
+        //     elevator_Subsystem.setHeight(0.6);
 
-        }));
+        // }));
         // opr.rightTrigger().onTrue(new InstantCommand(() -> {
         // elevator_Subsystem.setHeight(75.0);
         // }));
