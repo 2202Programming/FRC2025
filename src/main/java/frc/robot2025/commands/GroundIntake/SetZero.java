97c51c646fdb4598a37467259970284076f0ac5f
elena <elena.simone.rickert.2007@gmail.com>
2025-03-14 19:40:38 -0500

    something the compiler didnt catch when renaming stuff

--- src/main/java/frc/robot2025/commands/GroundIntake/SetZero.java
+++ src/main/java/frc/robot2025/commands/GroundIntake/SetZero.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot2025.commands.GroundIntake.Debug;
+package frc.robot2025.commands.GroundIntake;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.lib2202.builder.RobotContainer;
