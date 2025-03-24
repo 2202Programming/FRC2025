8e541cf0634dc0403d8dda90af58e138ab80c299
DerekL <derek@adminnet.us>
2025-03-24 16:12:33 -0500

    fix DriveToPickup and add pickup to Pos1 to Reef path

--- src/main/java/frc/robot2025/RegisteredCommands.java
+++ src/main/java/frc/robot2025/RegisteredCommands.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.lib2202.builder.RobotContainer;
 import frc.lib2202.command.swerve.RotateUntilSeeTags;
 import frc.robot2025.Constants.Tag_Pose;
@@ -36,7 +37,7 @@ public class RegisteredCommands {
             new InstantCommand(() -> {
                     elevator_Subsystem.setHeight(Levels.PickUp);
                 }),
-            new setWristPos(WristFLA.PICKUP_POSITION, "pickup")).withTimeout(5.0); //timeout for debug/sim
+            new setWristPos(WristFLA.PICKUP_POSITION, "pickup")).withTimeout(1.0); //timeout for debug/sim
     }
     private static Command place4(Levels level){
         return new SequentialCommandGroup(
@@ -45,7 +46,7 @@ public class RegisteredCommands {
             new setWristPos(0.6, "L4"), //position for L4 drop
             new ReleaseCoral(),
             new setWristPos(WristFLA.PICKUP_POSITION, "pickup").withTimeout(1.0),
-            new setElevatorSetpoint(Levels.PickUp)).withTimeout(5.0);  //timeout for debug/sim
+            new setElevatorSetpoint(Levels.PickUp)).withTimeout(1.0);  //timeout for debug/sim
     }
 
     public static void RegisterCommands() {
@@ -57,6 +58,8 @@ public class RegisteredCommands {
         NamedCommands.registerCommand("PlaceL2", place(Levels.LTwo));
         NamedCommands.registerCommand("PlaceL1", place(Levels.LOne));
         NamedCommands.registerCommand("PickupAdjustment", new PickupAdjustment());
+        // TODO - need a real pickup here
+        NamedCommands.registerCommand("WaitForPickup", new WaitCommand(3.0));  //TODO
         NamedCommands.registerCommand("Release", new PlaceSequence("coral", 83.0 ));
         NamedCommands.registerCommand("DriveToReefTagRight", new DriveToReefTag("r"));
         NamedCommands.registerCommand("DriveToReefTagLeft", new DriveToReefTag("l"));
